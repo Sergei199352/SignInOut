91957686f5e798eb211deebda125487278c96d7f
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-06-10 20:42:18 +0100

    the starter code

---
+++ server.js
@@ -0,0 +1,12 @@
+const ndef = new NDEFReader();
+var express = require('express')
+var app = express();
+
+ndef.scan().then(()=> {
+ndef.onreading = ({message, serialNumber})=>{
+    console.log(message)
+}
+})
+
+app.listen(8080);
+console.log('listening on 8080');

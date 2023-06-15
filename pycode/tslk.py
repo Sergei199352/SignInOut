0515dde48110d9e9fa79c335b90ae7178a46830c
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-15 12:02:05 +0100

    trying to deploy the website

--- pycode/tslk.py
+++ pycode/tslk.py
@@ -7,7 +7,7 @@ array = [1,2,3,4,5,6,7,8,9,10]
 data = {'array':array}
   
 # The POST request to our node server
-res = requests.post('20.90.226.171,20.90.226.197,20.90.226.238,20.90.227.32,20.90.227.42,20.90.227.94,20.90.134.24/arraysum', json=data) 
+res = requests.post('51.104.28.83/arraysum', json=data) 
   
 # Convert response data to json
 returned_data = res

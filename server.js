2af0759e712094412f9fe96cc826f15e08912797
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-17 11:47:14 +0100

    trying to get the post data displayed

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
   
 var app = express();
-
+app.use(express.static("public"))
 app.set('view engine', 'ejs');
   
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use(express.static("public"))
+
 
 let NFCdata = '';
 

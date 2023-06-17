d4e337b0a3ae3fbd15b573bf1b05a02b450d5824
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-17 11:50:53 +0100

    removing public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
   
 var app = express();
-app.use(express.static("public"))
+
 app.set('view engine', 'ejs');
   
 app.use(bodyParser.json());

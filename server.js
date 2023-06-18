142551247fa6f56a4cb4a2892186e113f2c6c8e4
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-18 14:43:32 +0100

    json work

--- server.js
+++ server.js
@@ -125,21 +125,21 @@ nfc = nfc1//
         var request = new sql.Request();
 
 
-        request.query("SELECT * FROM dbo.SignInOut WHERE rgu_id  = "+ nfc , function (err, recordset) {
+        request.query("SELECT * FROM dbo.SignInOut WHERE rgu_id AS json_data  = "+ nfc , function (err, recordset) {
             if (err) throw err;
             ressql.push = recordset;
             console.log(recordset)
 
-            for (let i = 0; i< ressql.recordsets.length; i++){
-                for(let j = 0; j<ressql.recordsets[i].length;j++){
-                    const rec = ressql.recordsets[i][j];
-                    for(let key in rec[key]){
-                        ajax={key:recordset[key]}
+            // for (let i = 0; i< ressql.recordsets.length; i++){
+            //     for(let j = 0; j<ressql.recordsets[i].length;j++){
+            //         const rec = ressql.recordsets[i][j];
+            //         for(let key in rec[key]){
+            //             ajax={key:recordset[key]}
 
-                    }
+            //         }
                     
-                }
-            }
+            //     }
+            
             console.log(ajax)
 
 

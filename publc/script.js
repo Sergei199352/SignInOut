3786017b9979354be67806cd5e4c8398a06bc3ed
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-22 16:11:37 +0100

    no rwfreshing

--- publc/script.js
+++ publc/script.js
@@ -13,20 +13,20 @@ $(document).ready(function() {
   });
   
   // Perform automatic page refresh after 10 seconds using AJAX
-  setInterval(function() {
-    // Perform AJAX request
-    $.ajax({
-      url: "https://rguappsign.azurewebsites.net/",
-      method: "GET",
-      success: function(response) {
-        // Handle the success response
+  // setInterval(function() {
+  //   // Perform AJAX request
+  //   $.ajax({
+  //     url: "https://rguappsign.azurewebsites.net/",
+  //     method: "GET",
+  //     success: function(response) {
+  //       // Handle the success response
         
-        // Reload the page
-        location.reload();
-      },
-      error: function(error) {
-        // Handle the error response
-      }
-    });
-  }, 10000);
+  //       // Reload the page
+  //       location.reload();
+  //     },
+  //     error: function(error) {
+  //       // Handle the error response
+  //     }
+  //   });
+  // }, 10000);
 });

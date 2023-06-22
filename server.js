926f82678b4c8be5f7ffa23c15b0b2f9ecc97a5d
Sergei199352 <Sergey.stanialvchuk@ik-worldwide.com>
2023-06-22 12:17:42 +0100

    rerros cor

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var nfc = "";
 var ressql = {};
 var data = [];
 app.set('view engine', 'ejs');
-  
+app.use(express.static('publc')) // adding publc
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
